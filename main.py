176a01add01d2663f50187e5c5b77ead8a8192c5
Yuta Takeuchi <yuta.takeuchi.31415@gmail.com>
2022-11-25 21:48:28 +0900

    add sampling button, change layout and refactor

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import dash
-from dash import dcc, html, callback_context, no_update
-from dash.dependencies import Input, Output
-import plotly.graph_objects as go
-import datetime
 import csv
+import datetime
 import os
+import subprocess
+
+import dash
+import plotly.graph_objects as go
+from dash import dcc, html, no_update
+from dash.dependencies import Input, Output
+
 current_date = str(datetime.datetime.now()).split(' ')[0]
 path = f'{current_date}.csv'
-#path = f'test.csv'
-
 last_mtime = os.stat(path).st_mtime
+
+
+def sampling():
+    return subprocess.check_output(['bash', 'sampling.sh'])
 
 
 def read_csv():
@@ -27,9 +32,11 @@ def get_co2_fig():
     fig = go.Figure()
     timestamp = [datetime.datetime.fromtimestamp(int(i)) for i in data[0][1:]]
     co2_ppm = [int(i) for i in data[1][1:]]
-    fig.add_trace(go.Scatter(x=timestamp, y=co2_ppm, line=dict(width=3)))
-    fig.update_layout(title='CO2 Monitor', xaxis_title='Date', yaxis_title='CO2 (PPM)')
-
+    fig.add_trace(go.Scatter(x=timestamp, y=co2_ppm, line=dict(width=2)))
+    fig.update_layout(title=dict(text='CO2 Monitor', xref='paper', xanchor='center', x=0.5),
+                      xaxis_title='Date',
+                      yaxis_title='CO2 (ppm)',
+                      margin=dict(l=200, r=200))
     return fig
 
 
@@ -37,25 +44,34 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Raspberry Pi Sensor Monitor'),
-    dcc.Graph(
-        id='co2-graph',
-        figure=get_co2_fig()
-    ),
+    html.Hr(),
+    html.H2(children='Raspberry Pi Sensor Monitor'),
+    html.Hr(),
+    html.H3(id='container-sample-main', children=''),
+    html.H6(id='container-sample-sub', children=''),
+    html.Button('Sampling', id='sampling', n_clicks=0),
+    html.Hr(),
+    dcc.Graph(id='co2-graph', figure=get_co2_fig()),
     dcc.Interval(id='interval', interval=60000, n_intervals=0)
-])
+], style={'textAlign': 'center'})
+
+
+@app.callback([Output('container-sample-main', 'children'), Output('container-sample-sub', 'children')],
+              Input('sampling', 'n_clicks'))
+def update_output_main(n_clicks):
+    sample = int(sampling())
+    return f'{sample} ppm', f'{datetime.datetime.now().strftime("%Y/%m/%d - %H:%M:%S")}'
 
 
 @app.callback(Output('co2-graph', 'figure'), [Input('interval', 'n_intervals')])
 def trigger_by_modify(n):
     global last_mtime
     if os.stat(path).st_mtime > last_mtime:
-        print("modified")
         last_mtime = os.stat(path).st_mtime
+        print(f'modified at {last_mtime}')
         return get_co2_fig()
     return no_update
 
 
 if __name__ == '__main__':
-    # `debug=True`でhot-reloadingモードを有効にし、コード上の変更を画面に反映する。
-    app.run_server(debug=True, host="0.0.0.0")
+    app.run_server(debug=False, host="0.0.0.0")
--- sampling.sh
+++ sampling.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 
-if [ ! -f ./latest.csv ] ; then
-  touch latest.csv
-  echo "creating latest.csv..."
-  echo "timestamp co2_ppm" >> latest.csv
-fi
+result=$(sudo python -m mh_z19 | cut -d " " -f 2 | sed 's/[^0-9]//g')
 
-current_date=$(date +"%Y-%m-%d")
-if [ ! -f ./"$current_date".csv ] ; then
-  touch "$current_date".csv
-  echo "creating $current_date.csv..."
-  echo "timestamp co2_ppm" >> "$current_date".csv
+if [ -z "$result" ]; then
+  echo "None"
+else
+  echo "$result"
 fi
-
-timestamp=$(date +%s)
-co2_ppm=$(sudo python -m mh_z19 | cut -d " " -f 2 | sed 's/[^0-9]//g')
-echo "$timestamp $co2_ppm" >> "$current_date".csv
