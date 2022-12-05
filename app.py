08e07786f00016302cecd9c8669cae9b91b6121d
Yuta Takeuchi <yuta.takeuchi.31415@gmail.com>
2022-12-05 21:50:25 +0900

    modify interval

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ app.layout = html.Div(children=[
             html.H6(id='shutdown_message', children='', style={'fontSize': 16}),
             html.Br()]),
     html.Img(src='assets/plotly_logo.webp', alt='image', style={'width': '12%', 'marginBottom': 40}),
-    dcc.Interval(id='interval', interval=10000, n_intervals=0)
+    dcc.Interval(id='interval', interval=20000, n_intervals=0)
 ], style={'textAlign': 'center', 'backgroundColor': 'WhiteSmoke', 'color': '#2F3F5C'})
 
 
